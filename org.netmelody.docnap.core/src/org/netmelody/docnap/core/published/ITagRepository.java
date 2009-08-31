891fad8db68c4971994147f6d311f8bf84362113
scarytom <none@none>
2009-08-31 09:48:41 +0100

    Add fetchAll to tag repository (refs #6).

--- org.netmelody.docnap.core/src/org/netmelody/docnap/core/published/ITagRepository.java
+++ org.netmelody.docnap.core/src/org/netmelody/docnap/core/published/ITagRepository.java
@@ -1,6 +1,7 @@
 package org.netmelody.docnap.core.published;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.netmelody.docnap.core.domain.Tag;
 
@@ -8,8 +9,11 @@ public interface ITagRepository {
 
     Collection<Tag> findByDocumentId(Integer identity);
 
+	List<Tag> fetchAll();
+    
     void tagDocumentById(Integer documentId, String tagTitle);
 
     void unTagDocumentById(Integer documentId, String tagTitle);
+
 
 }
--- org.netmelody.docnap.core/src/org/netmelody/docnap/core/repository/TagRepository.java
+++ org.netmelody.docnap.core/src/org/netmelody/docnap/core/repository/TagRepository.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.netmelody.docnap.core.domain.Tag;
 import org.netmelody.docnap.core.exception.DocnapRuntimeException;
@@ -18,23 +19,21 @@ public class TagRepository implements ITagRepository {
         this.connection = connection;
     }
     
-    public Collection<Tag> findByDocumentId(Integer identity) {
+    public List<Tag> fetchAll() {
+        final String sqlStmt = "SELECT tagid, creation_dt, title, description, count(l.documenttaglinkid)" +
+                               "  FROM TAGS t LEFT OUTER JOIN DOCUMENTTAGLINKS l" +
+                               "    ON (t.tagid = l.tagid)" +
+                               " GROUP BY tagid, creation_dt, title, description" +
+                               " ORDER BY count(l.documenttaglinkid) desc, title asc";
+        return fetchMultipleWithSql(sqlStmt);
+	}
+
+	public Collection<Tag> findByDocumentId(Integer identity) {
         final String sqlStmt = "SELECT tagid, creation_dt, title, description" +
                                "  FROM TAGS t INNER JOIN DOCUMENTTAGLINKS l" +
                                "    ON (t.tagid = l.tagid)" +
                                " WHERE l.documentid = " + identity;
-        final ResultSet resultSet = this.connection.executeSelect(sqlStmt);
-        final Collection<Tag> result = new ArrayList<Tag>();
-        try {
-            while(resultSet.next()) {
-                result.add(extractTag(resultSet));
-            }
-            resultSet.close();
-        }
-        catch (SQLException exception) {
-            throw new DocnapRuntimeException("Failed to retrieve documents", exception);
-        }
-        return result;
+        return fetchMultipleWithSql(sqlStmt);
     }
 
     public void tagDocumentById(Integer documentId, String tagTitle) {
@@ -71,9 +70,9 @@ public class TagRepository implements ITagRepository {
     public Tag fetchById(Integer identity) {
         final String sqlStmt = "SELECT tagid, creation_dt, title, description" +
         "  FROM TAGS WHERE tagid = " + identity;
-        return fetchWithSql(sqlStmt);
+        return fetchSingleWithSql(sqlStmt);
     }
-
+    
     private Tag createTag(String tagTitle) {
         if (tagExists(tagTitle)) {
             return fetchByTitle(tagTitle);
@@ -100,10 +99,25 @@ public class TagRepository implements ITagRepository {
     private Tag fetchByTitle(String tagTitle) {
         final String sqlStmt = "SELECT tagid, creation_dt, title, description" +
         "  FROM TAGS WHERE upper(title) = upper('" + tagTitle + "')";
-        return fetchWithSql(sqlStmt);
+        return fetchSingleWithSql(sqlStmt);
     }
 
-    private Tag fetchWithSql(String sqlStmt) {
+    private List<Tag> fetchMultipleWithSql(String sqlStmt) {
+    	final ResultSet resultSet = this.connection.executeSelect(sqlStmt);
+    	final List<Tag> result = new ArrayList<Tag>();
+    	try {
+    		while(resultSet.next()) {
+    			result.add(extractTag(resultSet));
+    		}
+    		resultSet.close();
+    	}
+    	catch (SQLException exception) {
+    		throw new DocnapRuntimeException("Failed to retrieve tags", exception);
+    	}
+    	return result;
+    }
+    
+    private Tag fetchSingleWithSql(String sqlStmt) {
         final ResultSet resultSet = this.connection.executeSelect(sqlStmt);
         try {
             if (!resultSet.next()) {
