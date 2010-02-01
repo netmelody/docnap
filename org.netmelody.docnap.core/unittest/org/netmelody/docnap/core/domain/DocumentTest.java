dd714b2c994a714a8af45f9972d1946b3f7d2428
Jennifer <none@none>
2010-02-01 21:51:57 +0000

    Added document unit tests

---
+++ org.netmelody.docnap.core/unittest/org/netmelody/docnap/core/domain/DocumentTest.java
@@ -0,0 +1,99 @@
+package org.netmelody.docnap.core.domain;
+
+import static org.junit.Assert.*;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.junit.Test;
+import org.netmelody.docnap.core.type.DocnapDateTime;
+
+public class DocumentTest {
+	
+	@Test
+	public void testInstantiationNull() {
+		final Document document = new Document();
+		
+		assertNotNull("New document should not be null", document);
+		assertNull("Title of new document should be null", document.getTitle());
+		assertNull("Date Added of new document should be null", document.getDateAdded());
+		assertNull("Basic Date Added of new document should be null", document.getDateAddedBasic());
+		assertNull("Original Filename of new document should be null", document.getOriginalFilename());
+		assertNull("Identity of new document should be null", document.getIdentity());
+	}
+	
+	@Test
+	public void testInstantiation() {
+		final Document document = new Document(5, "handle5");
+		
+		assertNotNull("New document should not be null", document);
+		assertNull("Title of new document should be null", document.getTitle());
+		assertNull("Date Added of new document should be null", document.getDateAdded());
+		assertNull("Original Filename of new document should be null", document.getOriginalFilename());
+		assertEquals("Identity of new document should be set", new Integer(5), document.getIdentity());
+	}
+	
+	private Document createDocument(Integer identity, String handle) {
+		return new Document(identity, handle);
+	}
+	
+	private Document createDocument() {
+		return createDocument(5, "jkl");
+	}
+
+	@Test
+	public void testTitle() {
+		final String newTitle = "NewTitle";
+		final Document document = createDocument();
+		
+		document.setTitle(newTitle);
+		assertEquals("Title should be set", newTitle, document.getTitle());
+	}
+
+	@Test
+	public void testDateAdded() {
+		final DocnapDateTime dateAdded = new DocnapDateTime(new Timestamp(4638458));
+		final Document document = createDocument();
+
+		
+		document.setDateAdded(dateAdded);
+		assertEquals("Date added should be set", dateAdded, document.getDateAdded());
+		assertEquals("Basic date should be equal", new Date(4638458), document.getDateAddedBasic());
+	}
+
+	@Test
+	public void testSetOriginalFilename() {
+    	final String originalFilename = "Original FileName.lst";
+		final Document document = createDocument();
+			
+		document.setOriginalFilename(originalFilename);
+		assertEquals("Original filename should be set", originalFilename, document.getOriginalFilename());
+	}
+
+	@Test
+	public void testToStringNoTitle() {
+		final Integer identity = new Integer(7);
+		final Document document = createDocument(identity, "thisisateapot");
+		
+		assertEquals("toString should return the identity", identity.toString(), document.toString());
+	}
+	
+	@Test
+	public void testToStringWithTitle() {
+		final Document document = createDocument(new Integer(6), "thisisateapot");
+		
+		final String title = "The dragon and the rock";
+		document.setTitle(title);
+		
+		assertEquals("toString should return the title", title, document.toString());
+	}
+
+	@Test
+	public void testGetIdentity() {
+		final Integer identity = new Integer(52);
+		final Document document = createDocument(identity, "thisisateapot");
+		
+		assertEquals("Identity should be same as set", identity, document.getIdentity());
+	}
+
+}
