b9d4ac8d6c73cd17df760e99db1faa9a65e7a530
Jennifer <none@none>
2010-02-06 20:24:42 +0000

    Added new test for two documents

--- org.netmelody.docnap.core/acceptancetest/org/netmelody/docnap/core/published/IndexTest.java
+++ org.netmelody.docnap.core/acceptancetest/org/netmelody/docnap/core/published/IndexTest.java
@@ -18,26 +18,30 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.netmelody.docnap.core.domain.Document;
 import org.netmelody.docnap.core.domain.Tag;
-import org.netmelody.docnap.core.repository.TagRepository;
 import org.picocontainer.PicoContainer;
 
 public class IndexTest {
 
     private static final String FILE_CONTENT = "Hello, world.";
     private static final String TAG_TITLE = "Sir Tag";
+    private static final String A_BORING_READ = "A boring read";
 
 	@Rule
     public TemporaryFolder folder = new TemporaryFolder();
     
     private File createTestFile() throws IOException {
+        return(createTestFile("myfile.txt", FILE_CONTENT));
+    }
+	
+	private File createTestFile(String fileName, String fileContent) throws IOException {
     	assertThat("TemporaryFolder root incorrect.", this.folder.getRoot(), is(notNullValue()));
     	
-    	final File testFile = this.folder.newFile("myFile.txt");
+    	final File testFile = this.folder.newFile(fileName);
     	final BufferedWriter out = new BufferedWriter(new FileWriter(testFile));
-        out.write(FILE_CONTENT);
+        out.write(fileContent);
         out.close();
         
-        assertThat("Incorrect test file setup.", FileUtils.readFileToString(testFile), is(FILE_CONTENT));
+        assertThat("Incorrect test file setup.", FileUtils.readFileToString(testFile), is(fileContent));
         
         return testFile;
     }
@@ -104,4 +108,51 @@ public class IndexTest {
 		
 		
 	}
+    
+    /**
+     * Create a new live docnap document store in a temp directory on the local file system.
+     * Add a temp file to it and a tag to the document
+     * Add a second file with no tag
+     * 
+     * Expect file content to be unchanged.
+     * 
+     * @throws IOException fail
+     */
+    @Test
+    public void testCreateNewDocnapStoreTwoDocumentsTagOne() throws IOException {
+        final File testFile = createTestFile();
+        
+        final PicoContainer context = new Bootstrap().start();
+        final IDocnapStore store = context.getComponent(IDocnapStore.class);
+        
+        final File newFolder = this.folder.newFolder("myStore");
+        store.setStorageLocation(newFolder.getCanonicalPath());
+        
+        final IDocumentRepository documentRepo = context.getComponent(IDocumentRepository.class);
+        final Document firstDocument = documentRepo.addDocument(testFile);
+        
+        final ITagRepository tagRepository = context.getComponent(ITagRepository.class);
+        tagRepository.tagDocumentById(firstDocument.getIdentity(), TAG_TITLE);
+        
+        final File secondTestFile = createTestFile("name.lst", "A boring read");
+        final Document secondDocument = documentRepo.addDocument(secondTestFile);
+        
+        Collection<Tag> documentTags = tagRepository.findByDocumentId(firstDocument.getIdentity());
+        assertEquals("Incorrect Number of Tags for document 1", 1, documentTags.size());
+        for (Tag tag : documentTags) {
+            assertNotNull("Tag should not be null", tag);
+            assertEquals("Tag title should be as specified", TAG_TITLE, tag.getTitle());
+        }
+        
+        documentTags = tagRepository.findByDocumentId(secondDocument.getIdentity());
+        assertEquals("Incorrect Number of Tags for document 2", 0, documentTags.size());
+        
+        final File retrievedFile = this.folder.newFile("myRetrievedFile.txt");
+        documentRepo.retrieveDocument(firstDocument, retrievedFile);
+        assertThat("Incorrect file content for document 1.", FileUtils.readFileToString(retrievedFile), is(FILE_CONTENT));
+        
+        final File retrievedSecondFile = this.folder.newFile("myRetrievedFile2.txt");
+        documentRepo.retrieveDocument(secondDocument, retrievedSecondFile);
+        assertThat("Incorrect file content for document 2.", FileUtils.readFileToString(retrievedSecondFile), is(A_BORING_READ));
+    }
 }
