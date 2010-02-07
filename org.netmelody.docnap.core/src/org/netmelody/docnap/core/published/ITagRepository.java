7d52c8fc2b18744bc4e9bd6ca6ee253e4ec95349
scarytom <none@none>
2010-02-07 00:00:28 +0000

    Tidy up tag repository interface.

--- org.netmelody.docnap.core/src/org/netmelody/docnap/core/published/ITagRepository.java
+++ org.netmelody.docnap.core/src/org/netmelody/docnap/core/published/ITagRepository.java
@@ -8,12 +8,13 @@ import org.netmelody.docnap.core.domain.Tag;
 public interface ITagRepository {
 
     Collection<Tag> findByDocumentId(Integer identity);
-
+    
+    Collection<Tag> findUnlinkedByDocumentId(Integer identity);
+    
     List<Tag> fetchAll();
     
     Tag tagDocumentById(Integer documentId, String tagTitle);
 
     void unTagDocumentById(Integer documentId, String tagTitle);
 
-    public Collection<Tag> findUnlinkedByDocumentId(Integer identity);
 }
