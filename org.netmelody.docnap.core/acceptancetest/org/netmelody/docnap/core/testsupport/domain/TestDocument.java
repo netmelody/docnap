8fe2154d5f61dbf3717ae94a1ce573c953abd762
scarytom <none@none>
2010-07-20 23:27:34 +0100

    remove unused import.

--- org.netmelody.docnap.core/acceptancetest/org/netmelody/docnap/core/testsupport/domain/TestDocument.java
+++ org.netmelody.docnap.core/acceptancetest/org/netmelody/docnap/core/testsupport/domain/TestDocument.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
-import org.netmelody.docnap.core.testsupport.checker.DocnapDocumentChecker;
 import org.netmelody.docnap.core.testsupport.checker.IDocnapTestConverter;
 
 public class TestDocument implements IDocnapTestConverter<DocnapDocument> {
