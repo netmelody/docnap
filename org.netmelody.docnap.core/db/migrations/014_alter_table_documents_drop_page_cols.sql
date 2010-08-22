f5630fecc20c9a09b24b6b82ab1a027b135c9349
scarytom <none@none>
2010-08-22 22:11:40 +0100

    correct typo in UNDO script.

--- org.netmelody.docnap.core/db/migrations/014_alter_table_documents_drop_page_cols.sql
+++ org.netmelody.docnap.core/db/migrations/014_alter_table_documents_drop_page_cols.sql
@@ -3,5 +3,5 @@ ALTER TABLE documents DROP COLUMN handle;
 
 --//@UNDO
 
-ALTER TABLE documents ADD COLUMN handle VARCHAR(2000),
+ALTER TABLE documents ADD COLUMN handle VARCHAR(2000);
 ALTER TABLE documents ADD CONSTRAINT uc_documents_handle UNIQUE (handle);
