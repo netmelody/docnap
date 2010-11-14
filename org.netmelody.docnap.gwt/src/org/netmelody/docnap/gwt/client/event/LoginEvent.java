5b4e73fca28a45720fa0f444a52aef9de79fd299
scarytom <none@none>
2010-11-14 12:36:32 +0000

    add example event and handler

---
+++ org.netmelody.docnap.gwt/src/org/netmelody/docnap/gwt/client/event/LoginEvent.java
@@ -0,0 +1,21 @@
+package org.netmelody.docnap.gwt.client.event;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+public class LoginEvent extends GwtEvent<LoginEventHandler> {
+
+    public static Type<LoginEventHandler> TYPE = new Type<LoginEventHandler>();
+
+    public LoginEvent() {
+    }
+
+    @Override
+    public Type<LoginEventHandler> getAssociatedType() {
+        return TYPE;
+    }
+
+    @Override
+    protected void dispatch(LoginEventHandler handler) {
+        handler.onLogin();
+    }
+}
---
+++ org.netmelody.docnap.gwt/src/org/netmelody/docnap/gwt/client/event/LoginEventHandler.java
@@ -0,0 +1,7 @@
+package org.netmelody.docnap.gwt.client.event;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface LoginEventHandler extends EventHandler {
+    void onLogin();
+}
